22badc777760121220c770dc087723190f0f2f29
henrychia12 <henry_chia21@hotmail.co.uk>
2019-01-16 17:33:17 +0000

    navbar and homepage imaage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './waterfall.jpg';
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Navbar/>
         <header className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+
+
         </header>
       </div>
     );
