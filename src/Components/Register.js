94fae6a46a35b3e778264b38cc61ec8545a3a824
henry <henry_chia21@hotmail.co.uk>
2019-01-21 20:18:30 +0000

    logo created on navbar. New components made - Register, login, home, asia and turkey

---
+++ src/Components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Register</h4>
+            <p>lorem</p>
+        </div>
+    )
+}
+
+export default Register;
---
+++ src/Components/Turkey.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Turkey = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Turkey</h4>
+            <p>lorem</p>
+        </div>
+    )
+}
+
+export default Turkey;
