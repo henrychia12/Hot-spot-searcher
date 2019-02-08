2bb86446f3b85d776aa76b159f629b9deb05fcde
henrychia12 <henry_chia21@hotmail.co.uk>
2019-02-08 15:46:53 +0000

    turkey locations can be read from the database to be presented on the page

--- src/Components/DeleteAccount.js
+++ src/Components/DeleteAccount.js
@@ -27,7 +27,7 @@ class DeleteAccount extends Component {
     clearSession = () => {
         sessionStorage.clear();
         this.props.history.push("/");
-      }
+    }
 
     handleAccountToDelete = (id) => {
         axios({
@@ -51,7 +51,7 @@ class DeleteAccount extends Component {
             console.log(userAccounts);
             for (let account = 0; account < userAccounts.length; account++) {
                 if ((this.state.userName === this.state.loggedInUser.userName) &&
-                 (this.state.password === this.state.loggedInUser.password)) {
+                    (this.state.password === this.state.loggedInUser.password)) {
                     this.handleAccountToDelete(this.state.loggedInUser.userID);
                 }
             }
@@ -62,33 +62,33 @@ class DeleteAccount extends Component {
     render() {
         return (
             <div className="register-background">
-            <div className="container-fluid bg-light py-5">
-                <div className="row">
-                    <div className="col-md-6 mx-auto">
-                        <div className="card card-body">
-                            <h3 className="text-center mb-4">Delete Account</h3>
-                            <div className="alert alert-danger">
-                                <a className="close font-weight-light" data-dismiss="alert">×</a>Please enter all fields to delete your account.
+                <div className="container-fluid bg-light py-5">
+                    <div className="row">
+                        <div className="col-md-6 mx-auto">
+                            <div className="card card-body">
+                                <h3 className="text-center mb-4">Delete Account</h3>
+                                <div className="alert alert-danger">
+                                    <a className="close font-weight-light" data-dismiss="alert">×</a>Please enter all fields to delete your account.
                 </div>
-                            <fieldset onSubmit={this.handleSubmit}>
-                                <div className="form-group has-error">
-                                    <input className="form-control input-lg" onChange={this.handleUserName} placeholder="Username" name="username" type="text" />
-                                </div>
-                                <div className="form-group has-success">
-                                    <input id="password" className="form-control input-lg" onChange={this.handlePassword} placeholder="Password" name="password" type="password" />
-                                </div>
-                                <div className="checkbox">
-                                    <label className="small">
-                                        <input name="terms" type="checkbox" />I confirm I wish to <a>delete my account</a>
-                                    </label>
-                                </div>
-                                <input className="btn btn-lg btn-primary btn-block" onClick={this.handleSubmit} value="Delete Account" type="submit" />
-                            </fieldset>
+                                <fieldset onSubmit={this.handleSubmit}>
+                                    <div className="form-group has-error">
+                                        <input className="form-control input-lg" onChange={this.handleUserName} placeholder="Username" name="username" type="text" />
+                                    </div>
+                                    <div className="form-group has-success">
+                                        <input id="password" className="form-control input-lg" onChange={this.handlePassword} placeholder="Password" name="password" type="password" />
+                                    </div>
+                                    <div className="checkbox">
+                                        <label className="small">
+                                            <input name="terms" type="checkbox" />I confirm I wish to <a>delete my account</a>
+                                        </label>
+                                    </div>
+                                    <input className="btn btn-lg btn-primary btn-block" onClick={this.handleSubmit} value="Delete Account" type="submit" />
+                                </fieldset>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
